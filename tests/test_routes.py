64c2a3e806aff3bc4c96de218f254d13f3069d41
Jakob <jboyelo@proton.me>
2025-08-26 14:01:15 -0400

    Update methods

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -125,6 +125,26 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        # create an Account to update
+        test_account = AccountFactory()
+        resp = self.client.post(
+            BASE_URL,
+            json=test_account.serialize()
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        new_account = resp.get_json()
+        new_account["name"] = "Test name"
+        resp = self.client.put(
+            f"{BASE_URL}/{new_account['id']}",
+            json=new_account
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "Test name")
+
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
         self._create_accounts(5)
